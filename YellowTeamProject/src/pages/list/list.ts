110ba3aaad09702d654c77cac80552935b531d6b
Menglei Lei <leo@dhcp-10-105-103-159.wireless.northwestern.private>
2017-03-30 14:34:33 -0500

    the page 2

--- YellowTeamProject/src/pages/list/list.ts
+++ YellowTeamProject/src/pages/list/list.ts
@@ -10,26 +10,65 @@ import { ItemDetailsPage } from '../item-details/item-details';
   templateUrl: 'list.html'
 })
 export class ListPage {
-  icons: string[];
-  items: Array<{title: string, note: string, icon: string}>;
 
+  hosts : Array<{name:string, location:string, connection:string, degree:string, intro:string, hostimg:string, sofaimg1:any, sofaimg2:any, sofaimg3:any}>
   constructor(public navCtrl: NavController, public navParams: NavParams) {
-    this.icons = ['flask', 'wifi', 'beer', 'football', 'basketball', 'paper-plane',
-    'american-football', 'boat', 'bluetooth', 'build'];
 
-    this.items = [];
-    for(let i = 1; i < 11; i++) {
-      this.items.push({
-        title: 'Item ' + i,
-        note: 'This is item #' + i,
-        icon: this.icons[Math.floor(Math.random() * this.icons.length)]
-      });
-    }
-  }
+    this.hosts=[
+      {
+       name:'Marty McFly',
+       location:"Evanston",
+       connection:'Menglei Lei',
+       degree:"2nd Degree",
+       intro:'some introduction to the sofa11',
+       hostimg:'../../assets/img/menglei.jpeg',
+       sofaimg1:'../../assets/img/sofa.JPG',
+       sofaimg2:'../../assets/img/sofa.JPG',
+       sofaimg3:'../../assets/img/sofa.JPG'
+     },
 
-  itemTapped(event, item) {
-    this.navCtrl.push(ItemDetailsPage, {
-      item: item
-    });
-  }
+      {
+       name:'AAAAA BBBBB',
+       location:"Los Angeles",
+       connection:'Menglei Lei',
+       degree:"1st Degree",
+       intro:'some introduction to the sofa22',
+       hostimg:'../../assets/img/menglei.jpeg',
+       sofaimg1:'../../assets/img/sofa.JPG',
+       sofaimg2:'../../assets/img/sofa.JPG',
+       sofaimg3:'../../assets/img/sofa.JPG'
+     },
+
+      {
+        name:'CCCCC DDDDD',
+        location:"San Jose",
+        connection:'Menglei Lei',
+        degree:"3rd Degree",
+        intro:'some introduction to the sofa33',
+        hostimg:'../../assets/img/menglei.jpeg',
+        sofaimg1:'../../assets/img/sofa.JPG',
+        sofaimg2:'../../assets/img/sofa.JPG',
+        sofaimg3:'../../assets/img/sofa.JPG'
+      },
+
+      {
+        name:'EEEEE FFFFFF',
+        location:"Chicago",
+        connection:'Menglei Lei',
+        degree:"4th Degree",
+        intro:'some introduction to the sofa44',
+        hostimg:'../../assets/img/menglei.jpeg',
+        sofaimg1:'../../assets/img/sofa.JPG',
+        sofaimg2:'../../assets/img/sofa.JPG',
+        sofaimg3:'../../assets/img/sofa.JPG'
+      }
+
+    ];
+
+  // itemTapped(event, item) {
+  //   this.navCtrl.push(ItemDetailsPage, {
+  //     item: item
+  //   });
+  // }
+}
 }
