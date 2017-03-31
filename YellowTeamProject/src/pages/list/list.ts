77c9a58726c270b97a637905f3b7f2130ca02e64
lilingkai <li.kevin295@gmail.com>
2017-03-30 21:41:31 -0500

    Attempted Google Maps Autocomplete Places API, failed

--- YellowTeamProject/src/pages/list/list.ts
+++ YellowTeamProject/src/pages/list/list.ts
@@ -1,74 +1,58 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ViewController } from 'ionic-angular';
 
-import { NavController, NavParams } from 'ionic-angular';
-
-import { ItemDetailsPage } from '../item-details/item-details';
-
+declare var google: any;
 
 @Component({
-  selector: 'page-list',
-  templateUrl: 'list.html'
+    selector: 'page-list',
+    templateUrl: 'list.html'
 })
-export class ListPage {
+export class ListPage implements OnInit{
 
-  hosts : Array<{name:string, location:string, connection:string, degree:string, intro:string, hostimg:string, sofaimg1:any, sofaimg2:any, sofaimg3:any}>
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+    autocompleteItems: any;
+    autocomplete: any;
+    acService:any;
+    placesService: any;
 
-    this.hosts=[
-      {
-       name:'Marty McFly',
-       location:"Evanston",
-       connection:'Menglei Lei',
-       degree:"2nd Degree",
-       intro:'some introduction to the sofa11',
-       hostimg:'../../assets/img/menglei.jpeg',
-       sofaimg1:'../../assets/img/sofa.JPG',
-       sofaimg2:'../../assets/img/sofa.JPG',
-       sofaimg3:'../../assets/img/sofa.JPG'
-     },
+    constructor(public viewCtrl: ViewController) { 
+    }
 
-      {
-       name:'AAAAA BBBBB',
-       location:"Los Angeles",
-       connection:'Menglei Lei',
-       degree:"1st Degree",
-       intro:'some introduction to the sofa22',
-       hostimg:'../../assets/img/menglei.jpeg',
-       sofaimg1:'../../assets/img/sofa.JPG',
-       sofaimg2:'../../assets/img/sofa.JPG',
-       sofaimg3:'../../assets/img/sofa.JPG'
-     },
+    ngOnInit() {
+        this.acService = new google.maps.places.AutocompleteService();        
+        this.autocompleteItems = [];
+        this.autocomplete = {
+            query: ''
+        };        
+    }
 
-      {
-        name:'CCCCC DDDDD',
-        location:"San Jose",
-        connection:'Menglei Lei',
-        degree:"3rd Degree",
-        intro:'some introduction to the sofa33',
-        hostimg:'../../assets/img/menglei.jpeg',
-        sofaimg1:'../../assets/img/sofa.JPG',
-        sofaimg2:'../../assets/img/sofa.JPG',
-        sofaimg3:'../../assets/img/sofa.JPG'
-      },
+    dismiss() {
+        this.viewCtrl.dismiss();
+    }
 
-      {
-        name:'EEEEE FFFFFF',
-        location:"Chicago",
-        connection:'Menglei Lei',
-        degree:"4th Degree",
-        intro:'some introduction to the sofa44',
-        hostimg:'../../assets/img/menglei.jpeg',
-        sofaimg1:'../../assets/img/sofa.JPG',
-        sofaimg2:'../../assets/img/sofa.JPG',
-        sofaimg3:'../../assets/img/sofa.JPG'
-      }
+    chooseItem(item: any) {
+        console.log('modal > chooseItem > item > ', item);
+        this.viewCtrl.dismiss(item);
+    }
 
-    ];
+    updateSearch() {
+        console.log('modal > updateSearch');
+        if (this.autocomplete.query == '') {
+            this.autocompleteItems = [];
+            return;
+        }
+        let self = this;
+        let config = { 
+            types:  ['geocode'], // other types available in the API: 'establishment', 'regions', and 'cities'
+            input: this.autocomplete.query, 
+            componentRestrictions: { country: 'AR' } 
+        }
+        this.acService.getPlacePredictions(config, function (predictions, status) {
+            console.log('modal > getPlacePredictions > status > ', status);
+            self.autocompleteItems = [];            
+            predictions.forEach(function (prediction) {              
+                self.autocompleteItems.push(prediction);
+            });
+        });
+    }
 
-  // itemTapped(event, item) {
-  //   this.navCtrl.push(ItemDetailsPage, {
-  //     item: item
-  //   });
-  // }
-}
 }
